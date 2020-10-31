93a7d00b24332acf015daa89200a767eb2e5fd2a
El-Chapo0133 <levequeloris66@gmail.com>
2020-10-31 16:08:48 +0100

    switched from 'random_from_scratch' to 'random_rust', added round_f_to_i() func and units test, all done

--- src/main.rs
+++ src/main.rs
@@ -2,49 +2,47 @@ use std::time::SystemTime;
 
 
 mod data {
+	// max i64 value
 	pub const MAX: i128 = 922337203685775807;
+	// imperative choosen value
 	pub const MULTIPLICATOR: i64 = 16807;
-
 }
 
 struct Random {
-	buffer: i64,
+	seed: i64, // seed used to generate the random, based on unix time and imperatives values
 }
 impl Random {
-	fn new() -> Random {
+	/*pub*/ fn new() -> Random {
 		Random {
-			buffer: (get_time_unix() * data::MULTIPLICATOR + 1) % (power_of(2,31) - 1),
+			seed: (get_time_unix() * data::MULTIPLICATOR + 1) % (power_of(2,31) - 1),
 		}
 	}
-	fn next(&mut self, min: i64, max: i64) -> i64 {
+	// use this function to get the next random
+	/*pub*/ fn next(&mut self, min: i64, max: i64) -> i64 {
 		round_f_to_i((min + (max - min)) as f64 * self.pseudo_uniform())
 	}
 	fn pseudo_uniform(&mut self) -> f64 {
 		let modulo: i64 = power_of(2,31) - 1;
-		self.buffer = (self.buffer * data::MULTIPLICATOR + 1) % modulo;
-		return self.buffer as f64 / modulo as f64;
-	}
-}
-struct Vector {
-	data: [i64],
-}
-impl Vector {
-	fn new() -> Vector {
-		Vector {
-			data: [],
-		}
-	}
-	fn push(&mut self, input: i64) {
-		
+		self.seed = (self.seed * data::MULTIPLICATOR + 1) % modulo; // re-set the seed
+		return self.seed as f64 / modulo as f64;
 	}
 }
 
 fn main() {
 	let mut random: Random = Random::new();
-	println!("{:.0}", random.next(0, 1));
+	println!("{}", random.next(0, 1));
+
+	
+	// generate 100 randoms
+	for _ in 0..100 {
+		println!("{:.0}", random.next(0, 1));
+	}
+	
 }
 
-
+/// get the power of a number from the exposant
+/// panic! if the number is too big
+/// shouldn't append, cuz i only make 2^31
 fn power_of(base: i64, exposant: i64) -> i64 {
 	let mut to_return: i128 = base as i128;
 	for _ in 1..exposant {
@@ -56,40 +54,25 @@ fn power_of(base: i64, exposant: i64) -> i64 {
 	return to_return as i64;
 }
 fn round_f_to_i(input: f64) -> i64 {
-	let stringed = format!("{:.2}", input);
-	let splitted = split_once(stringed);
-}
-fn split_once(input: String) -> [&str;2] {
-	let char_to_find = ".";
-	let to_return = [""; 2];
-	for 
-}
-fn to_cells(input: String) -> 
-fn length(input: String) -> i64 {
-
-}
-fn to_string(input: i64) -> Option<&'static str> {
-	let chars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
-	let ints = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-	for i in 0..11 {
-		if ints[i] == input {
-			return Some(chars[i]);
-		}
+	let mut index: i64 = 0; // which index do we are
+	loop {
+		index += 1;
+		let next_dec: i64 = get_dec(&input, index); // get the dec at the index given
+		if next_dec != 5 {
+			if next_dec < 5 {
+				return input as i64;
+			} else {
+				return input as i64 + 1;
+			}
+		} // if next_dec == 5, make another turn
 	}
-	None
 }
-fn to_int(input: &str) -> Option<i64> {
-	let chars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
-	let ints = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-	for i in 0..11 {
-		if chars[i] == input {
-			return Some(ints[i]);
-		}
-	}
-	None
+// get the index decimal
+fn get_dec(input: &f64, index: i64) -> i64 {
+	(input * (power_of(10, index)) as f64) as i64 % 10
 }
 
-
+// get the unix time
 fn get_time_unix() -> i64 {
 	match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
 		Ok(n) => return n.as_secs() as i64,
@@ -107,7 +90,7 @@ mod tests {
 
 	#[test]
 	fn test_power_of() {
-		let mut inputs: Vec<i64> = [1,2,3,4,5].to_vec();
+		let inputs: Vec<i64> = [1,2,3,4,5].to_vec();
 		let mut outputs: Vec<i64> = [1,4,9,16,25].to_vec();
 		for i in 0..inputs.len() {
 			assert_eq!(power_of(inputs[i],2), outputs[i]);
@@ -118,19 +101,29 @@ mod tests {
 		}
 	}
 	#[test]
-	fn test_to_string() {
-		let inputs: Vec<i64> = [1,2,3,4,5,11].to_vec();
-		let outputs: Vec<Option<&'static str>> = [Some("1"),Some("2"),Some("3"),Some("4"),Some("5"),None].to_vec();
+	fn test_get_dec() {
+		let mut inputs: Vec<f64> = [1.1, 1.2, 0.4, 1245.7].to_vec();
+		let mut outputs: Vec<i64> = [1,2,4,7].to_vec();
 		for i in 0..inputs.len() {
-			assert_eq!(to_string(inputs[i]), outputs[i]);
+			assert_eq!(get_dec(&inputs[i], 1), outputs[i]);
+		}
+		inputs = [1.12, 1.25, 0.41, 1245.447].to_vec();
+		outputs = [2,5,1,4].to_vec();
+		for i in 0..inputs.len() {
+			assert_eq!(get_dec(&inputs[i], 2), outputs[i]);
+		}
+		inputs = [1.125, 15.25235, 0.4112, 1245.4347].to_vec();
+		outputs = [5,2,1,4].to_vec();
+		for i in 0..inputs.len() {
+			assert_eq!(get_dec(&inputs[i], 3), outputs[i]);
 		}
 	}
 	#[test]
-	fn test_to_int() {
-		let inputs: Vec<&str> = ["1","2","3","4","5",".","10"].to_vec();
-		let outputs: Vec<Option<i64>> = [Some(1),Some(2),Some(3),Some(4),Some(5),Some(10),None].to_vec();
+	fn test_round_f_to_i() {
+		let inputs: Vec<f64> = [125.6, 12.73, 123.556, 0.555551, 0.555559].to_vec();
+		let outputs: Vec<i64> = [126,13,124,0,1].to_vec();
 		for i in 0..inputs.len() {
-			assert_eq!(to_int(inputs[i]), outputs[i]);
+			assert_eq!(round_f_to_i(inputs[i]), outputs[i]);
 		}
 	}
 }
